2707b7d280de34f5294e6d27e494f3f9dce6c65e
gunesgokhan <138656079+gunesgokhan@users.noreply.github.com>
2025-07-08 22:46:18 +0300

    Update productService.js

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 // src/services/productService.js
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://product-app-h6ik.onrender.com/';
 
 export const getProducts = async (filters = {}) => {
     try {
